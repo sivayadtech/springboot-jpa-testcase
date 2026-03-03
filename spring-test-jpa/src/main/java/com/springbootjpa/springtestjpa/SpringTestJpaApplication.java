64ffc5a8d4e88fcc6ccb8e8495a30415fb317ad8
Cvamyname <161030466+Cvamyname@users.noreply.github.com>
2026-03-03 22:51:36 +0530

    Initial project artifacts

---
+++ spring-test-jpa/src/main/java/com/springbootjpa/springtestjpa/SpringTestJpaApplication.java
@@ -0,0 +1,13 @@
+package com.springbootjpa.springtestjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestJpaApplication.class, args);
+    }
+
+}
---
+++ spring-test-jpa/src/test/java/com/springbootjpa/springtestjpa/SpringTestJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootjpa.springtestjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
